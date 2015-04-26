2b0953c8cf67f1ec24e718dae5a564aacc022c2e
halzhang <zhg_grj@163.com>
2015-04-26 18:12:03 +0800

    添加注释；配置混淆。

--- RxExample/app/proguard-rules.pro
+++ RxExample/app/proguard-rules.pro
@@ -15,3 +15,6 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+
+-dontwarn retrofit.**
+-keep class retrofit.** { *; }
--- RxExample/app/src/main/java/com/halzhang/android/example/rxexample/ApiManager.java
+++ RxExample/app/src/main/java/com/halzhang/android/example/rxexample/ApiManager.java
@@ -8,23 +8,30 @@ import rx.Subscriber;
 import rx.schedulers.Schedulers;
 
 /**
+ * 接口
  * Created by Hal on 15/4/26.
  */
 public class ApiManager {
 
     private static final String ENDPOINT = "http://api.openweathermap.org/data/2.5";
 
+    /**
+     * 服务接口
+     */
     private interface ApiManagerService {
-
         @GET("/weather")
         WeatherData getWeather(@Query("q") String place, @Query("units") String units);
-
     }
 
     private static final RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(ENDPOINT).setLogLevel(RestAdapter.LogLevel.FULL).build();
 
     private static final ApiManagerService apiManager = restAdapter.create(ApiManagerService.class);
 
+    /**
+     * 将服务接口返回的数据，封装成{@link rx.Observable}
+     * @param city
+     * @return
+     */
     public static Observable<WeatherData> getWeatherData(final String city) {
         return Observable.create(new Observable.OnSubscribe<WeatherData>() {
             @Override
